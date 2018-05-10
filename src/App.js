d9aacfef709c4afb760639dc0ca9048ed56da1a7
Darek Chynek <dariuszchynek90@gmail.com>
2018-05-10 15:51:09 +0200

    Add Login container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Login from './Containers/Login/Login';
 import Class from './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <div className={Class.title}>Triptic</div>
+        <Login></Login>
+      </div>
     );
   }
 }
---
+++ src/Containers/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Class from './Login.scss'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className={Class.title}>Triptic Login Page</div>
+    );
+  }
+}
+
+export default Login;
