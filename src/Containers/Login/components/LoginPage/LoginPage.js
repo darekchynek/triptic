3b56b83328a941ab5b0fc13f0ba1b78191e5f6e2
Darek Chynek <dariuszchynek90@gmail.com>
2018-05-10 18:15:49 +0200

    login png

--- src/Containers/Login/components/LoginPage/LoginPage.js
+++ src/Containers/Login/components/LoginPage/LoginPage.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Classes from './LoginPage.scss';
+import React from "react";
+import Classes from "./LoginPage.scss";
+import logo from "../../../../assets/driver-logo.png";
 
 const LoginPage = props => {
-    return (
-        <div className={Classes.container}>
-            <div className={Classes.loginContainer}>
-              <div id={Classes.output}></div>
-              <div className={Classes.avatar}></div>
-              <div className={Classes.formBox}>
-                      <input name="user" type="text" placeholder="username"></input>
-                      <input type="password" placeholder="password"></input>
-                      <button className={Classes.btnLogin}>Login</button>
-                      <p className={Classes.forgotPassword}>If You forgot Your Password, Click here</p>
-              </div>
-          </div>
+  return (
+    <div className={Classes.container}>
+      <div className={Classes.loginContainer}>
+        <div id={Classes.output} />
+        <img src={logo} alt="Logo" className={Classes.avatar}/>
+        <div className={Classes.formBox}>
+          <input name="user" type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button className={Classes.btnLogin}>Login</button>
+          <p className={Classes.forgotPassword}>
+            If You forgot Your Password, Click here
+          </p>
         </div>
-      );
-}
-    
-
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
